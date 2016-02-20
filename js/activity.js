a96540186c6b37f2374cc27fae95e9493310fbfc
erilyth <vishal.batchu@students.iiit.ac.in>
2016-02-20 18:35:06 +0530

    Base commit

---
+++ js/activity.js
@@ -0,0 +1,12 @@
+
+var requestAnimationFrame = window.requestAnimationFrame ||
+window.mozRequestAnimationFrame ||
+window.webkitRequestAnimationFrame ||
+window.msRequestAnimationFrame;
+
+//Use classes for creating various objects
+//OOP principles
+//Syntax checks - rules
+
+this.textElem = document.getElementById('text');
+this.textElem.innerHTML = "Changed text using JS";
